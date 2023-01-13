20428a9dced0131dcfb49eeebd847777b3d3a372
Gabo Dev <gonzaloramirez.sagitario@gmail.com>
2023-01-12 21:48:14 -0300

    The contact section was created

--- src/js/animations.js
+++ src/js/animations.js
@@ -4,6 +4,7 @@ const cardTools          = document.getElementById('card__tools');
 const cardCertifications = document.getElementById('card__certifications');
 const cardProyectsOne    = document.getElementById('card__proyects--one');
 const cardProyectsTwo    = document.getElementById('card__proyects--two');
+const contact            = document.getElementById('contact__box');
 
 const showCard = (entradas)=>{
 
@@ -35,3 +36,4 @@ observe.observe(cardTools);
 observe.observe(cardCertifications);
 observe.observe(cardProyectsOne);
 observe.observe(cardProyectsTwo);
+observe.observe(contact);
