d363ac8c880dc36e4a25fb9b0a1d9450901d283b
GaboDev <96387359+gabodev-ar@users.noreply.github.com>
2023-01-25 20:53:08 -0300

    Phpmailer disabled
    Configuración requerida para el despliegue en githubpages

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import {} from "./nav.js";
 import {} from "./scrolling.js";
-import {} from "./phpMailer.js";
+//import {} from "./phpMailer.js";
 import {Button} from "./Button.js"
 
 document.querySelector('.button--download').addEventListener('click', ()=>{
